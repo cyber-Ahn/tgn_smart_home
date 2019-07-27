99959d855b56ea47166393c88145674c26781c65
cyber Ahn <35652262+cyber-Ahn@users.noreply.github.com>
2019-07-27 10:22:41 +0200

    Add files via upload

---
+++ libs/tgn_sonoff.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+import sys
+
+sonoff_homecode = "10101"
+sonoff_topic = "tgn/sonoff/data"
+
+def tgn_sonoff(topic, homecode, modul, channel, status, mqtt_ip):
+    print("send")
+    client = mqtt.Client("sonoff Modul")
+    client.connect(mqtt_ip)
+    client.loop_start()
+    data_send = homecode + "-" + modul + "." + channel + "-" + status
+    print(data_send)
+    client.publish(topic,data_send,qos=0,retain=True)
+
+i_modul = sys.argv[1]
+i_channel = sys.argv[2]
+i_status = sys.argv[3]
+i_ip = sys.argv[4]
+tgn_sonoff(sonoff_topic,sonoff_homecode,i_modul,i_channel,i_status,i_ip)
+
+#python3 tgn_sonoff.py 1 0 1 192.168.0.98
+#python3 tgn_sonoff.py modul channel on/off mqtt-ip
