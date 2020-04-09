0f809476ae666625d3f826d9243f803e482996b0
cyber Ahn <35652262+cyber-Ahn@users.noreply.github.com>
2020-04-09 20:44:45 +0200

    update

--- mcserver.py
+++
@@ -1,9 +0,0 @@
-from mcstatus import MinecraftServer
-def mc_check(ipadd):
-    server = MinecraftServer.lookup(ipadd)
-    try:
-        status = server.status()
-        print("Server Online")
-    except:
-        print("Server Offline")
-mc_check("192.168.0.90")
